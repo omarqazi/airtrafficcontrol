2c5884dd57b24540a31ed5fa40b2b151414032f5
Omar Qazi <omar@smick.co>
2018-09-30 16:16:12 -0700

    implemented controller actions for placing an order and getting the status of an order

--- controller/order_controller.go
+++ controller/order_controller.go
@@ -25,11 +25,31 @@ type OrderController struct {
 }
 
 func (oc OrderController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	requestUrl := r.URL.Path
+
+	if len(requestUrl) == 0 && r.Method == "GET" {
+		oc.OrderQueueLength(w, r)
+	} else if r.Method == "POST" {
+		oc.TakeOrder(w, r)
+	} else if r.Method == "GET" {
+		oc.GetOrderStatus(w, r)
+	}
+}
+
+func (oc OrderController) OrderQueueLength(w http.ResponseWriter, r *http.Request) {
 	ordersWaiting, err := redisClient.LLen("smick-drone-orders").Result()
 	if err != nil {
 		fmt.Fprintln(w, "error talking to redis")
 		return
 	}
 
-	fmt.Fprintln(w, "orders in line:", ordersWaiting)
+	fmt.Fprintln(w, "orders in queue:", ordersWaiting)
+}
+
+func (oc OrderController) TakeOrder(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "your order is coming right up")
+}
+
+func (oc OrderController) GetOrderStatus(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "uhhh haha yeah we are working on it")
 }
